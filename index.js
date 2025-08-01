1929944c72f28eaef474259e80515c9a413c89d3
Fredricknyirenda <fredricknyirendat@gmail.com>
2025-08-01 16:20:59 +0200

    added toggle

--- index.js
+++ index.js
@@ -5,3 +5,21 @@
 // TODO: Add code for reset button
 
 // TODO: Add code for adding new list items from form
+@media(prefers - color - scheme: dark) {
+  :root {
+        --bg - color: #121212;
+        --text - color: #ffffff;
+    }
+}
+
+@media(color: light) {
+  :root {
+        --bg - color: #ffffff;
+        --text - color: #121212;
+    }
+}
+
+$('.btn').click(function () {
+    $('.btn').toggle(
+    );
+});
